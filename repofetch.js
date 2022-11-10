83ea86ba0cb9900e057fa43e30758abf555c2a76
Furkan Abay <furkan_abay@hotmail.com>
2022-11-10 15:26:51 +0100

    Changed css and js for better looks

--- repofetch.js
+++ repofetch.js
@@ -1,6 +1,6 @@
 const url = "https://api.github.com/users/furreabay/repos"
-const leftUl = document.querySelector(".left-repo-ul");
-const rightUl = document.querySelector(".right-repo-ul");
+const repoUl = document.querySelector(".repo-ul");
+
 
 
 async function fetchApi() {
@@ -20,20 +20,11 @@ async function fetchApi() {
         if (obj.name === obj.owner.login) {
             return;
         }
-
-        if (leftLi.length > rightLi.length) {
-            rightUl.innerHTML += `<li class="right-li">${obj.name}<br>
+        repoUl.innerHTML += `<li class="right-li">${obj.name}<br>
             <p class"github-description">${obj.description}</p><br>
             <a href="${obj.html_url}" class="github-link">Click to get to the github repo!<a/></li>`
-            console.log(obj.name)
-            console.log(obj.html_url)
-        } else {
-            leftUl.innerHTML += `<li class="left-li">${obj.name}<br>
-            <p class"github-description">${obj.description}</p><br>
-            <a href="${obj.html_url}" class="github-link">Click to get to the github repo!<a/></li>`
-            console.log(obj.name)
-            console.log(obj.html_url)
-        }
+        console.log(obj.name)
+        console.log(obj.html_url)
     });
 
 }
